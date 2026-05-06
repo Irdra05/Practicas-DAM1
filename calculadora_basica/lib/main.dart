adf9ef4e2bd5707e4a5725a8a998acf6e6be9cf6
Irdra05 <adri.ortiz.martin@gmail.com>
2026-05-06 17:12:23 +0200

    wordle de prueba - Layout.3

---
+++ calculadora_basica/lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+import 'game.dart';
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  Widget build(BuildContext content) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Align(
+            alignment: Alignment.centerLeft,
+            child: Text('Birdle'),
+          ),
+        ),
+        body: Center(
+          child: Tile('A', HitType.hit),
+        ),
+      ),
+    );
+  }
+}
+
+class Tile extends StatelessWidget {
+  const Tile(this.letter, this.hitType, {super.key});
+
+  final String letter;
+  final HitType hitType;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 60,
+      width: 60,
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey.shade300),
+        color: switch (hitType) {
+          HitType.hit => Colors.green,
+          HitType.partial => Colors.yellow,
+          HitType.miss => Colors.grey,
+          _ => Colors.white
+        } 
+      ),
+      child: Center(
+        child: Text(
+          letter.toUpperCase(),
+          style: Theme.of(context).textTheme.titleLarge,
+        ),
+      ),
+    );
+  }
+}
